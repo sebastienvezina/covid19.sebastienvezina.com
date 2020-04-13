56a9767902ef5851b48746061f4cf31669a5f688
Sebastien Vezina <info@groundcontrolaudio.com>
2020-04-13 00:51:34 -0400

    can now change the y scale

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,23 @@
 var chart;
 var apiUrl = "https://covid19-api.sebastienvezina.com/json";
+var ymin, ymax;
+
+$('.btn-pos-scale').click(function(e){
+    $('.btn-pos-scale').removeClass('active').addClass('not-active');
+   
+    var btn = $(this);
+    //make current button active
+    btn.removeClass('not-active').addClass('active');
+    scale = btn.attr('data-pos-scale');
+    if (scale == "rel") {
+        chart.options.scales.yAxes[0].ticks.min = ymin; //Math.min(...chartData.datasets[0].data) + 0.1;
+        chart.options.scales.yAxes[0].ticks.max = ymax; //Math.max(...chartData.datasets[0].data);
+    }else {
+        chart.options.scales.yAxes[0].ticks.min = 0;
+        chart.options.scales.yAxes[0].ticks.max = 4;
+    }
+    chart.update()
+});
 
 $('.btn-chart-mode').click(function(e){
     //desactivate all buttons
@@ -56,119 +74,133 @@ $('.btn-chart-mode').click(function(e){
         chart.options.annotation.annotations[0].value = mean; 
         chart.options.annotation.annotations[0].label.content = meanString + Number(mean).toFixed(2);
         chart.update()
-    });
-
-});
-
-$.getJSON(apiUrl + "?mode=24", function(data) {  
-    chartData = {
-        labels: [],
-        datasets: [
-            {
-                label: 'COVID-19 news sentiment over time',
-                borderColor: 'rgb(200, 0, 0)',
-                gridLines: {
-                    color: '#ddd'
-                },
-                data: []
-            }
-        ]
-    }
-    labels = [];
-    values = [];
-    $.each(data, function(key, value){
-        labels.push(value[0]);
-        values.push(value[1]);
-    });
-    chartData.labels = labels;
-    chartData.datasets[0].data = values;
-    
-    //get mean
-    mean = math.mean(values);
-    
-    var ctx = document.getElementById('chart').getContext('2d');
-    chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: chartData,
-        // Configuration options go here
-        options: {
-            legend: {
-                display: false,
-            },
-            scales: {
-                yAxes: [
-                    {
-                        gridLines: {
-                            color: '#333'
-                        },
-                        ticks: {
-                            callback: function(label, index, labels) {
-                                return Number(label).toFixed(2);
-                            },
-                            fontColor: '#eee',
-                            fontSize: 14,
-                        },
-                        
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Positivity (0 is more negative, 4 is more positive)',
-                            fontColor: '#eee',
-                            fontSize: 16,
-                        }
-                    }
-                ],
-                xAxes: [
-                    {
-                        gridLines: {
-                            color: '#333'
-                        },
-                        ticks: {
-                            maxTicksLimit:24,
-                            callback: function(label, index, labels) {
-                                var d = new Date(label);
-                                var dateTimeOptions = {}
-                                dateTimeOptions = {
-                                    month: 'short',
-                                    day: '2-digit',
-                                    hour: '2-digit',
-                                    minute: '2-digit',
-                                    hour12: false
-                                };
-                                out = new Intl.DateTimeFormat('en-US', dateTimeOptions).format(d);
-
-                                return out;
-                            },
-                            fontColor: '#eee',
-                            fontSize: 14,
-                        },
-                        
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Last 24 hours EDT',
-                            fontColor: '#eee',
-                            fontSize: 16,
-                        }
-                    }
-                ]
         
-            },
-            annotation: {
-                annotations: [{
-                  type: 'line',
-                  mode: 'horizontal',
-                  scaleID: 'y-axis-0',
-                  value: mean,
-                  borderColor: '#eee',
-                  borderWidth: 2,
-                  label: {
-                    enabled: true,
-                    content: "24 hours mean: " + Number(mean).toFixed(2)
-                  }
-                }]
-              }
+        //keep ticks values
+        if ($('.btn-pos-scale .active').attr('data-pos-scale') == 'rel') {
+            ymax = chart.options.scales.yAxes[0].ticks.max;
+            ymin = chart.options.scales.yAxes[0].ticks.min;  
         }
     });
+
 });
+
+function init() {
+    $.getJSON(apiUrl + "?mode=24", function(data) {  
+        chartData = {
+            labels: [],
+            datasets: [
+                {
+                    label: 'COVID-19 news sentiment over time',
+                    borderColor: 'rgb(200, 0, 0)',
+                    gridLines: {
+                        color: '#ddd'
+                    },
+                    data: []
+                }
+            ]
+        }
+        labels = [];
+        values = [];
+        $.each(data, function(key, value){
+            labels.push(value[0]);
+            values.push(value[1]);
+        });
+        chartData.labels = labels;
+        chartData.datasets[0].data = values;
+        
+        //get mean
+        mean = math.mean(values);
+        
+        var ctx = document.getElementById('chart').getContext('2d');
+        chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+    
+            // The data for our dataset
+            data: chartData,
+            // Configuration options go here
+            options: {
+                legend: {
+                    display: false,
+                },
+                scales: {
+                    yAxes: [
+                        {
+                            gridLines: {
+                                color: '#333'
+                            },
+                            ticks: {
+                                callback: function(label, index, labels) {
+                                    return Number(label).toFixed(2);
+                                },
+                                fontColor: '#eee',
+                                fontSize: 14,
+                                
+                            },
+                            
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Positivity (0 is more negative, 4 is more positive)',
+                                fontColor: '#eee',
+                                fontSize: 16,
+                            }
+                        }
+                    ],
+                    xAxes: [
+                        {
+                            gridLines: {
+                                color: '#333'
+                            },
+                            ticks: {
+                                maxTicksLimit:24,
+                                callback: function(label, index, labels) {
+                                    var d = new Date(label);
+                                    var dateTimeOptions = {}
+                                    dateTimeOptions = {
+                                        month: 'short',
+                                        day: '2-digit',
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                        hour12: false
+                                    };
+                                    out = new Intl.DateTimeFormat('en-US', dateTimeOptions).format(d);
+    
+                                    return out;
+                                },
+                                fontColor: '#eee',
+                                fontSize: 14,
+                            },
+                            
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Last 24 hours EDT',
+                                fontColor: '#eee',
+                                fontSize: 16,
+                            }
+                        }
+                    ]
+            
+                },
+                annotation: {
+                    annotations: [{
+                      type: 'line',
+                      mode: 'horizontal',
+                      scaleID: 'y-axis-0',
+                      value: mean,
+                      borderColor: '#eee',
+                      borderWidth: 2,
+                      label: {
+                        enabled: true,
+                        content: "24 hours mean: " + Number(mean).toFixed(2)
+                      }
+                    }]
+                  }
+            }
+        });
+        
+        //keep ticks values
+        ymax = chart.options.scales.yAxes[0].ticks.max;
+        ymin = chart.options.scales.yAxes[0].ticks.min;
+    });
+}
+init();
